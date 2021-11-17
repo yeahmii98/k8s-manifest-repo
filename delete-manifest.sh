e52244863d8e502515d988c04f064030ac640817
huhji <huhji.elha@gmail.com>
2021-11-17 15:00:10 +0900

    add apply & delete manifest

---
+++ delete-manifest.sh
@@ -0,0 +1,5 @@
+kubectl delete -f plate-detection-deployment.yaml
+kubectl delete -f plate-detection-service.yaml
+kubectl delete -f backend-deployment.yaml
+kubectl delete -f backend-service.yaml
+kubectl delete -f ingress.yaml
